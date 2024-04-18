998cffd6b7fc38b0cd9347a1266514392bfaf35b
Sushixnoodle <145032867+Sushixnoodle@users.noreply.github.com>
2024-04-18 12:45:25 +0100

    8 18

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public List<Item> Items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+
+
+}
