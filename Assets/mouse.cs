fbad8efda1f83529f28f66d21b4bb0c07ba226dd
Sushoma1 <sdas2023@elam.co.uk>
2024-05-13 15:50:19 +0100

    dhgiu

--- Assets/mouse.cs
+++ Assets/mouse.cs
@@ -7,13 +7,8 @@ public class mouse : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.None;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
