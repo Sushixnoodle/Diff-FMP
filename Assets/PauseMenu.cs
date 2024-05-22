5ce86eee3b65f955d11dfda104f90f8797d61839
Sushixnoodle <145032867+Sushixnoodle@users.noreply.github.com>
2024-05-22 14:22:45 +0100

    FSFWA

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-   /* public GameObject pauseMenu; 
+    public GameObject pauseMenu; 
     
     public static bool isPaused;
     
@@ -46,5 +46,5 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-*/
+
 }
